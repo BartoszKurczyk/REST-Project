204379616a42f55c4c352e347aeed2213b9e9832
kb39334 <kb39334@zut.edu.pl>
2021-03-24 17:16:24 +0100

    Some fixes

--- src/main/java/com/example/demo/model/SetNameRequest.java
+++ src/main/java/com/example/demo/model/SetNameRequest.java
@@ -1,2 +1,12 @@
-package com.example.demo.model;public class SetNameRequest {
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SetNameRequest {
+    private String name;
 }
--- src/main/java/com/example/demo/web/TestController.java
+++ src/main/java/com/example/demo/web/TestController.java
@@ -1,4 +1,23 @@
 package com.example.demo.web;
 
+import com.example.demo.model.SetNameRequest;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+
+    @GetMapping(path = "/name")
+    public String getName()
+    {
+        return "Bartosz";
+    }
+    @PostMapping(value = "/name")
+    public String setName(@Validated @RequestBody(required = true) SetNameRequest setNameRequest)
+    {
+        return setNameRequest.getName();
+    }
 }
